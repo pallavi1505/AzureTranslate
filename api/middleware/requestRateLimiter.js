f67e98b8fe74d3da8733a29fd2d1fdc8dd616135
pallavi1505 <pallavishirodkar1505@gmail.com>
2022-12-11 07:42:14 +0000

    added final report

--- api/middleware/requestRateLimiter.js
+++ api/middleware/requestRateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const requestRateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 15 min in milliseconds
-  max: 10,
+  max: 30,
   message: "Error: You have reached maximum requests. Please try again after 30 minutes", 
   statusCode: 429,
   headers: true,
